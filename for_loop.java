63ae7de6aab419e96190643b1d985afe14fc0781
vanthiyadhevan <samvickey96@gmail.com>
2023-10-17 16:12:58 +0530

    Third Commit

--- for_loop.java
+++ for_loop.java
@@ -7,7 +7,9 @@
  */
 public class for_loop {
     public static void main(String args) {
+        //Outer loop 
         for(int i = 1; i <= 5; i++) {
+            //inner loop
             for(int j = 1; j <= 5; j++) {
                 System.out.print(j);
             }
